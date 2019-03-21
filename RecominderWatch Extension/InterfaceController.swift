adc42bbd940e2bc7899f6c73ff828bb4b96ee15e
Erica Naglik <40673456+ericanaglik@users.noreply.github.com>
2019-03-20 22:50:14 -0700

    new ui added

--- RecominderWatch Extension/InterfaceController.swift
+++ RecominderWatch Extension/InterfaceController.swift
@@ -13,7 +13,12 @@ import Foundation
 class InterfaceController: WKInterfaceController {
 
     @IBOutlet weak var runningLabel: WKInterfaceLabel!
-    @IBOutlet weak var heartRateLabel: WKInterfaceLabel!
+    
+    @IBOutlet weak var addWaterButton: WKInterfaceButton!
+    @IBOutlet weak var heartRatePanel: WKInterfaceImage!
+    @IBOutlet weak var caloresPanel: WKInterfaceImage!
+    @IBOutlet weak var waterPanel: WKInterfaceImage!
+    @IBOutlet weak var consumptionLabel: WKInterfaceImage!
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -24,7 +29,15 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
         runningLabel.setText("Running")
-        heartRateLabel.setTextColor(#colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1))
+        
+//        func scroll(to: WKInterfaceImage, at scrollPosition: WKInterfaceScrollPosition, animated: Bool) {
+//
+//        }
+//        scroll(to: heartRatePanel, at: WKInterfaceScrollPosition.centeredVertically, animated: true)
+//        scroll(to: caloresPanel, at: WKInterfaceScrollPosition.centeredVertically, animated: true)
+//        scroll(to: waterPanel, at: WKInterfaceScrollPosition.centeredVertically, animated: true)
+//        scroll(to: consumptionLabel, at: WKInterfaceScrollPosition.centeredVertically, animated: true)
+//        heartRateLabel.setTextColor(#colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1))
     }
     
     
