bd5e68b455e0ae9beb0e6c1251c28e33cc77fa6a
Erica Naglik <40673456+ericanaglik@users.noreply.github.com>
2019-03-23 18:04:11 -0700

    fixed logic error

---
+++ RecominderWatch Extension/ThirdInterfaceController.swift
@@ -0,0 +1,14 @@
+//
+//  ThirdInterfaceController.swift
+//  RecominderWatch Extension
+//
+//  Created by ✨💖Erica Naglik💖✨ on 3/23/19.
+//  Copyright © 2019 Timofey Makhlay. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class ThirdInterfaceController: WKInterfaceController {
+    
+}
